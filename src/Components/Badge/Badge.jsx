6e1ec9bf429d53384a6fd4a49986728767cd4d5c
parvezmahmudlalin <parvezmahmudlalin@gmail.com>
2026-04-13 04:40:53 +0600

    fix badge component design and divider

--- src/Components/Badge/Badge.jsx
+++ src/Components/Badge/Badge.jsx
@@ -4,21 +4,33 @@ import React from 'react';
 const Badge = () => {
   return (
    
-     <div className='bg-purple-700 p-9 grid grid-cols-3 items-center justify-evenly text-center mt-15'>
-      <div >
-        <h1 className='font-extrabold text-5xl text-white'>50K+</h1>
-        <p className='text-white items'>Active Users</p>
-      </div>
-         
-      <div >
-        <h1 className='font-extrabold text-5xl text-white'>200+</h1>
-        <p className='text-white items'>Premium Tools</p>
-      </div>
+        <div className="bg-linear-to-r from-[#4F39F6] to-[#9514FA] p-10">
+      
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 text-center">
+        
+      
+        <div className="py-6 relative">
+          <h1 className="text-white text-5xl font-extrabold">50K+</h1>
+          <p className="text-white/80 mt-2">Active Users</p>
+
+          <div className="hidden md:block absolute right-0 top-1/2 -translate-y-1/2 h-20 w-[1px] bg-white/30"></div>
+        </div>
+
        
-      <div >
-        <h1 className='font-extrabold text-5xl text-white'>4.9</h1>
-        <p className='text-white items'>Rating</p>
+        <div className="py-6 relative">
+          <h1 className="text-white text-5xl font-extrabold">200+</h1>
+          <p className="text-white/80 mt-2">Premium Tools</p>
+
+          <div className="hidden md:block absolute right-0 top-1/2 -translate-y-1/2 h-20 w-[1px] bg-white/30"></div>
+        </div>
+
+        <div className="py-6">
+          <h1 className="text-white text-5xl font-extrabold">4.9</h1>
+          <p className="text-white/80 mt-2">Rating</p>
+        </div>
+
       </div>
+
     </div>
    
   );
